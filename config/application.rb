4aaffd265bcb59cb2342a74cce50b8de2e0cb600
Pinaguchi <skgchi919@gmail.com>
2022-03-20 07:46:52 +0000

    [karennda- ]

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module RunningRoutine
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Asia/Tokyo'
+
+    config.time_zone = 'Tokyo'
   end
 end
