23c086d55f06b0365b07acbaa5e590d23019aa07
Pinaguchi <skgchi919@gmail.com>
2022-03-23 14:43:08 +0000

    [update]

--- db/schema.rb
+++ db/schema.rb
@@ -63,20 +63,6 @@ ActiveRecord::Schema.define(version: 2022_03_23_112714) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "runs", force: :cascade do |t|
-    t.datetime "start_at"
-    t.datetime "stoped_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "time_records", force: :cascade do |t|
-    t.datetime "start_time"
-    t.datetime "stoped_time"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
